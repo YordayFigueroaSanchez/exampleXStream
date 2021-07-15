01104a6d1bade9298ead70c8bdaafc4372eabd47
Yorday Figueroa <yfsanchez@gmail.com>
2021-07-15 01:26:31 -0300

    write file xml
    write file xml

--- src/main/java/xstream/inicio/Principal.java
+++ src/main/java/xstream/inicio/Principal.java
@@ -1,9 +1,38 @@
 package xstream.inicio;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.thoughtworks.xstream.XStream;
+
+import xstream.entity.Person;
+import xstream.entity.PhoneNumber;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		XStream xstream = new XStream();
+		
+		xstream.alias("person", Person.class);
+		xstream.alias("phonenumber", PhoneNumber.class);
+		
+		Person joe = new Person("Joe", "Walnes");
+		joe.setPhone(new PhoneNumber(123, "1234-456"));
+		joe.setFax(new PhoneNumber(123, "9999-999"));
+		
+		String xml = xstream.toXML(joe);
+		
+		try {
+		      FileWriter myWriter = new FileWriter("filename.xml");
+		      myWriter.write(xml);
+		      myWriter.close();
+		      System.out.println("Successfully wrote to the file.");
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
 
 	}
 
